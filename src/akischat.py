d940a1f41ebcee70681915eed318a0295db1d22d
Stephen Akiki <stephen.akiki@gmail.com>
2009-10-15 13:32:09 -0400

    Added Debug and SendText function

--- src/akischat.py
+++ src/akischat.py
@@ -43,16 +43,47 @@
 # Copyright (C) 2009 Stephen Akiki. All rights reserved.
 #-------------------------------------------------------------------------------------------------
 
+import os, thread, socket, traceback
 
 #-------------------CONSTANTS-------------------------
 
-LOCAL_IP = socket.gethostbyname(socket.gethostname())
+LOCAL_IP = socket.gethostbyname(socket.gethostname()) # Gets local IP address
 
-IP_ADDRESS_LIST = []
+IP_ADDRESS_LIST = [] # Holds all the IP addresses
 
 PORT = 7721 # Port to send packets on
 
+DEBUG = 1
 #-----------------------------------------------------
+
+# Used to print out info that I need during debugging.
+def dbg(string):
+	global DEBUG
+	if DEBUG == 1:
+		print '-----DEBUG-----: ' + str(string)
+	else:
+		pass
+
+# This is a wrapper function so we can abstract away how we print the characters, either to the 
+#   command line or the GUI
+def PrintToScreen(str):
+	print str
+
+
+def SendText(str):
+	global PORT
+	for ip_addr in IP_ADDRESS_LIST:
+		try:
+			d = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			d.sendto(str, (ip_addr, PORT))
+			d.close()
+		except:
+			PrintToScreen('Could not send to: ' + ip_addr)
+			dbg((str, ip_addr, PORT))
+			if DEBUG == 1:
+				traceback.print_exc()
+			else:
+				pass
 
 
 
